96d3af86aed39b0229415e62e2ca2059ca8066fc
MadinaK <m.kuldeeva06@mail.ru>
2025-01-12 16:50:50 +0500

    feat: PostgresSteps

---
+++ src/bdd/java/com/personal_expense_tracker/main/steps/PostgresSteps.java
@@ -0,0 +1,66 @@
+package com.personal_expense_tracker.main.steps;
+
+import com.personal_expense_tracker.main.model.Expense;
+import com.personal_expense_tracker.main.repository.ExpenseRepository;
+import com.personal_expense_tracker.main.utils.DatabaseConnection;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+
+public class PostgresSteps {
+
+    private Connection connection;
+    private ExpenseRepository expenseRepository;
+
+    static final int EXPENSE_FIXTURE_1_ID = 100;
+    static final String EXPENSE_FIXTURE_1_DESCRIPTION = "Description A";
+    static final double EXPENSE_FIXTURE_1_AMOUNT = 1000000.0;
+    static final String EXPENSE_FIXTURE_1_CATEGORY = "Category A";
+    static final String EXPENSE_FIXTURE_1_DATE = "2025-01-11";
+
+    static final int EXPENSE_FIXTURE_2_ID = 200;
+    static final String EXPENSE_FIXTURE_2_DESCRIPTION = "Description B";
+    static final double EXPENSE_FIXTURE_2_AMOUNT = 2000000.0;
+    static final String EXPENSE_FIXTURE_2_CATEGORY = "Category B";
+    static final String EXPENSE_FIXTURE_2_DATE = "2025-01-12";
+
+    @Before
+    public void setUp() throws SQLException {
+        connection = DatabaseConnection.connect();
+        expenseRepository = new ExpenseRepository(connection);
+        try (Statement stmt = connection.createStatement()) {
+            stmt.execute("DELETE FROM expenses");
+        }
+    }
+
+    @Given("The database contains a few orders")
+    public void the_database_contains_a_few_orders() {
+        addTestExpenseToDatabase(EXPENSE_FIXTURE_1_ID, EXPENSE_FIXTURE_1_DESCRIPTION,
+                EXPENSE_FIXTURE_1_AMOUNT, EXPENSE_FIXTURE_1_CATEGORY, EXPENSE_FIXTURE_1_DATE);
+        addTestExpenseToDatabase(EXPENSE_FIXTURE_2_ID, EXPENSE_FIXTURE_2_DESCRIPTION,
+                EXPENSE_FIXTURE_2_AMOUNT, EXPENSE_FIXTURE_2_CATEGORY, EXPENSE_FIXTURE_2_DATE);
+    }
+
+    private void addTestExpenseToDatabase(int id,
+                                          String description,
+                                          double amount,
+                                          String category,
+                                          String date) {
+        try {
+            Expense expense = new Expense();
+            expense.setId(id);
+            expense.setDescription(description);
+            expense.setAmount(amount);
+            expense.setCategory(category);
+            expense.setDate(LocalDate.parse(date));
+            expenseRepository.saveExpense(expense);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
--- src/main/java/com/personal_expense_tracker/main/repository/ExpenseRepository.java
+++ src/main/java/com/personal_expense_tracker/main/repository/ExpenseRepository.java
@@ -1,6 +1,7 @@
 package com.personal_expense_tracker.main.repository;
 
 import com.personal_expense_tracker.main.model.Expense;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,28 +13,55 @@ public class ExpenseRepository {
     public ExpenseRepository(Connection connection) {
         this.connection = connection;
     }
-    
+
     public void createTableIfNotExists() throws SQLException {
         String sql = "CREATE TABLE IF NOT EXISTS expenses (" +
-                     "id SERIAL PRIMARY KEY, " +
-                     "description TEXT, " +
-                     "category VARCHAR(100) NOT NULL, " +
-                     "amount DECIMAL(10, 2) NOT NULL, " +
-                     "date DATE NOT NULL)";
+                "id SERIAL PRIMARY KEY, " +
+                "description TEXT, " +
+                "category VARCHAR(100) NOT NULL, " +
+                "amount DECIMAL(10, 2) NOT NULL, " +
+                "date DATE NOT NULL)";
         try (Statement stmt = connection.createStatement()) {
             stmt.execute(sql);
         }
     }
 
     public void saveExpense(Expense expense) throws SQLException {
-        String sql = "INSERT INTO Expenses (description, category, amount, date) VALUES (?, ?, ?, ?)";
+        String sql;
+
+        // Проверяем, передан ли id
+        if (expense.getId() != 0 && expense.getId() > 0) {
+            // Если id передан, используем его в запросе
+            sql = "INSERT INTO Expenses (id, description, category, amount, date) VALUES (?, ?, ?, ?, ?)";
+        } else {
+            // Если id не передан, база данных сгенерирует его автоматически
+            sql = "INSERT INTO Expenses (description, category, amount, date) VALUES (?, ?, ?, ?)";
+        }
         try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-            stmt.setString(1, expense.getDescription());
-            stmt.setString(2, expense.getCategory());
-            stmt.setDouble(3, expense.getAmount());
-            stmt.setDate(4, Date.valueOf(expense.getDate()));
+            // Если id передан, устанавливаем его
+            if (expense.getId() != 0 && expense.getId() > 0) {
+                stmt.setLong(1, expense.getId());
+                stmt.setString(2, expense.getDescription());
+                stmt.setString(3, expense.getCategory());
+                stmt.setDouble(4, expense.getAmount());
+                stmt.setDate(5, Date.valueOf(expense.getDate()));
+            } else {
+                // Если id не передан, устанавливаем только остальные параметры
+                stmt.setString(1, expense.getDescription());
+                stmt.setString(2, expense.getCategory());
+                stmt.setDouble(3, expense.getAmount());
+                stmt.setDate(4, Date.valueOf(expense.getDate()));
+            }
             stmt.executeUpdate();
         }
+//        String sql = "INSERT INTO Expenses (description, category, amount, date) VALUES (?, ?, ?, ?)";
+//        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+//            stmt.setString(1, expense.getDescription());
+//            stmt.setString(2, expense.getCategory());
+//            stmt.setDouble(3, expense.getAmount());
+//            stmt.setDate(4, Date.valueOf(expense.getDate()));
+//            stmt.executeUpdate();
+//        }
     }
 
     public List<Expense> getAllExpenses() throws SQLException {
